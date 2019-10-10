57634654045433b5bbb8c41032958be9aa484374
Sergey Lamcev <s.lamcev@newlinetechnologies.net>
2019-10-10 10:35:43 +0300

    Initial commit

---
+++ VideoTransparentBackground/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  VideoTransparentBackground
+//
+//  Created by Andrey Gordeev on 6/11/17.
+//  Copyright © 2017 Andrey Gordeev (andrew8712@gmail.com). All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
